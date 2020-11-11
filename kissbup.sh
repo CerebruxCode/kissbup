12a4368e020ddf9dccf94af83f17532351bfb222
Salih Emin <cerebrux@users.noreply.github.com>
2020-11-11 19:00:05 +0200

    Update kissbup.sh

--- kissbup.sh
+++ kissbup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#   (c)2020 Kissbup | Salih Emin
+#   LICENSED under Apache 2.0 
 
-SOURCE_DIR="/home/username/BackupThis"
-DEST_DIR="/media/diskos/kissbups/"
-NAME_BACKUP="mybackup-$(date +%d-%m-%Y)"
-BACKUP_NUM="10"
+SOURCE_DIR="/home/username/BackupThis" # Τι κάνω backup
+DEST_DIR="/media/diskos/kissbups/" # Που κάνω backup
+NAME_BACKUP="mybackup-$(date +%d-%m-%Y)" # Τι όνομα να έχει το backup
+BACKUP_NUM="10" # Συνολικά backup που πρέπει να διατηρούνται εάν δημιουργείται αντίγραφα καθημερινά
 
+# Συμπίεση backups
 tar -zcpvf "$DEST_DIR"/"$NAME_BACKUP".tar.gz "$SOURCE_DIR"
+# Αναζητήσε αντίγραφα ασφαλείας παλαιότερα από BACKUP_NUM και διαγράψτε τα
 find "$DEST_DIR"/* -mtime +$BACKUP_NUM -exec rm {} \;
